f4f55cc9c9187a8b26a32e48fa5ecf5b2b07469a
zhoudaxia2016 <1207190489@qq.com>
2017-04-28 21:50:42 +0800

    修改服务器配置

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/logout',function(req,res){
 });
 
 
-var server = app.listen(9000, function () {
+var server = app.listen(8004, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
